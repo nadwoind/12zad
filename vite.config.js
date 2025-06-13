8efd9873cad67d58105af7182c9e0eb0a32e2bde
Maciej Filek <maciejfilek62@gmail.com>
2025-06-13 22:07:57 +0200

    P

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 export default ({ mode }) => {
   loadEnv(mode, process.cwd());
   return defineConfig({
+    base: '/12zad/',             
     define: { 'process.env': {} },
     server: { port: 3000 },
     resolve: { alias: { '@': path.resolve(__dirname, './src') } },
