b446dfe9c1306b686ffd113b2895bc839f3c8a86
Maciej Filek <maciejfilek62@gmail.com>
2025-06-14 09:16:59 +0200

    dzialaj

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+const supabaseUrl = 'https://fxvqfsxpmnwnenzztlel.supabase.co'  
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4dnFmc3hwbW53bmVuenp0bGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0MTY0ODYsImV4cCI6MjA2NDk5MjQ4Nn0.TYKW--AfJzYHsEQgodcA2o2VKUoRvMdKM5KeHm-eZvY' 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
